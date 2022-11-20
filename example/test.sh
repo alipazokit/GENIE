a78b132920074f7a007731de9aada1c28e8344db
alipazokit <46820262+alipazokit@users.noreply.github.com>
2022-11-19 20:12:41 -0800

    Update test.sh

--- example/test.sh
+++ example/test.sh
@@ -5,7 +5,7 @@ annot=single.annot
 env=test.env
 
 
-../build/GENIE   -g $gen -p $phen   -e $env  -k 10 -jn 10   -o test.out.txt -annot $annot
+../build/GENIE   -g $gen -p $phen -c $covar  -e $env  -k 10 -jn 10   -o test.out.txt -annot $annot
 
 
 
